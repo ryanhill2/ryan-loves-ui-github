8a069c320b794268c7277c10b4da3117838414f3
Ryan Hill <ryanhill00101@gmail.com>
2024-06-28 18:05:27 +0100

    Adding new run file

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Run npm build
+npm run build
+
+# Copy build contents to root
+cp -r build/* .
+
+# Add all files
+git add .
+
+# Commit changes with a commit message
+git commit -m "Adding new run file"
+
+# Push changes to remote repository
+git push
